1743b3a6ecd4d7c5b31f1cc0fb6ca89c1c3b5f57
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-23 18:07:30 -0500

    Added missing pep layout

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     memory_dict = {}
-    def wrapper(*args, **kwargs):
+    
+    def wrapper(*args, **kwargs) -> int:
         if args in memory_dict:
             return memory_dict[args]
         else:
             result = func(args)
             memory_dict[args] = result
             return result
-    return wrapper 
+    return wrapper
