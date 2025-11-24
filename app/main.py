88f7ad2971c7cf34424cfa1b8fd82dcbd34564ac
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-24 18:34:55 -0500

    Unpacking values passed to the function

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     memory_dict = {}
     
-    def wrapper(*args, **kwargs) -> int:
+    def wrapper(*args) -> int:
         if args in memory_dict:
-            return memory_dict[args]
+            return memory_dict[*args]
         else:
-            result = func(args)
+            result = func(*args)
             memory_dict[args] = result
             return result
     return wrapper
