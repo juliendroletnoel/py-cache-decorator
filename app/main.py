929186b07ed87de230826f0ab4e6efe6f7e8c3ff
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-24 18:44:59 -0500

    Remove the asteris when accessing the dictionnary

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> int:
         if args in memory_dict:
             print("Getting from cache")
-            return memory_dict[*args]
+            return memory_dict[args]
         else:
             print("Calculating new result")
             result = func(*args)
